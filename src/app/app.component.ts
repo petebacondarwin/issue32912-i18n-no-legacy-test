424fa704c1f21645566884eaffd33f5c85be553e
Pete Bacon Darwin <pete@bacondarwin.com>
2020-09-18 08:33:47 +0100

    disable legacy message ids

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'i18n-no-legacy-test';
+  greeting = $localize`Hello!`;
 }
